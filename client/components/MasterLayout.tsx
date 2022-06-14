ca281581ca6129aa5521f0c811f9e4d7813a8b37
Ali Zaynoune <alzaynou@e3r9p1.1337.ma>
2022-06-14 14:12:21 +0100

    button primary&ghost done && add icons
    Signed-off-by: Ali Zaynoune <alzaynou@e3r9p1.1337.ma>

--- client/components/MasterLayout.tsx
+++ client/components/MasterLayout.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import { ReactNode } from "react";
 
 import Header from "./Header";
+// import UserIcon from '../public/assets/icons/user.svg'
 
 type Props = {
   children: ReactNode;
@@ -14,13 +15,11 @@ const MasterLayout = ({ children }: Props) => {
       <Header />
       {children}
       {/* <div className="container p-10 d-flex"> */}
-        <button className="btn-ghost m-5">button</button>
-        <button className="button">button</button>
-      {/* </div> */}
-      <br />
-      {/* <label htmlFor="currency" className="sr-only"></label> */}
-      {/* <input className="input-default" placeholder="test" /> */}
-      {/* <label htmlFor="currency" className=""></label> */}
+      <button className="btn-ghost m-5 userInfo loading" disabled type="button">
+        button1
+      </button>
+      <button className="btn-primary m-5  userInfo loading">button</button>
+     
       <div className="container p-10">
         <div className="form-item">
           <div className="input-error">
