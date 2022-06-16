a2b173fb2efb27842a1314d2b993ceb2a70e9fab
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-06-16 19:06:32 +0100

    start designing master layout
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- client/components/MasterLayout.tsx
+++ client/components/MasterLayout.tsx
@@ -1,12 +1,14 @@
 import { NextComponentType } from "next";
+import Image from "next/image";
 import { AppProps } from "next/app";
 import { ReactNode } from "react";
-// import Header from "./Header";
-import Button from './Button'
-import Input from './Input'
-import {Layout} from 'antd'
+import { Layout, Button, Typography } from "antd";
+import { Eye, EyeSlash } from "../public/assets/icons/EyeIcon";
+import { PoweroffOutlined } from "@ant-design/icons";
+import Icon from "@ant-design/icons";
+import layoutStyle from "../styles/layout.module.css";
 
-const { Header, Footer, Sider, Content} = Layout
+const { Header, Footer, Content } = Layout;
 
 type Props = {
   children: ReactNode;
@@ -15,9 +17,28 @@ type Props = {
 const MasterLayout = ({ children }: Props) => {
   return (
     <Layout>
-      <Header>Header</Header>
-      <Content>Content</Content>
-      <Footer>Footer</Footer>
+      <Header className={layoutStyle.header}>
+        <Image src="/images/Logo.png" height={60} width={100} />
+        <Button>SignIn</Button>
+      </Header>
+      <Content>{children}</Content>
+      <div className={layoutStyle.sectionGameInfo}>
+        <Image src="/images/Logo.png" height={60} width={100} />
+        <div className={layoutStyle.sectionGameInfoText}>
+          <Typography.Title level={4}>
+            About game
+          </Typography.Title>
+          <Typography.Text >
+          Ping-pong is a game in which two or four players hit a light, hollow ball back and forth across a net stretched across the center of a table. The game is more commonly known as table tennis, reflecting its origin as an indoor modification of the sport of lawn tennis. The term ping-pong is a federally registered trademark for the game first issued to Parker Brothers, Inc., in 1901, and now owned by Escalade Sports, of Evansville, Indiana.
+
+          Provide additional interactive capacity of editable and copyable.
+          </Typography.Text>
+        </div>
+      </div>
+      <Footer className={layoutStyle.footer}>
+        <span>Ft_transcendence</span>
+        <span>© 2022 1337. All rights reserved.</span>
+      </Footer>
     </Layout>
   );
 };
