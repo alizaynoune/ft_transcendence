bca60f36105b43e83e0eb35bdc400cb6a2e034cd
alizaynoune <zaynoune.ali@gmail.com>
2022-06-26 21:45:36 +0100

    profile level-progress done
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- client/containers/userStatistics/Statistics.tsx
+++ client/containers/userStatistics/Statistics.tsx
@@ -1,11 +1,17 @@
 import style from "./statistics.module.css";
 import { NextComponentType } from "next";
 import Image from "next/image";
-import { Progress, Avatar } from "antd";
+import { Progress, Avatar, Tooltip } from "antd";
 import { useSession } from "next-auth/react";
+import { UserOutlined, AntDesignOutlined } from "@ant-design/icons";
+
+const level = 12.70;
 
 const Statistics: React.FC = () => {
   const { data: session } = useSession();
+  const progress = (((level - Math.floor(level))) / 1) * 100;
+  // console.log(progress);
+
   return (
     <div className={style.container}>
       <div className={style.progressContainer}>
@@ -14,10 +20,32 @@ const Statistics: React.FC = () => {
           src="/images/defaultProfileAvatar.jpg"
           objectFit="cover"
           layout="fill"
-        //   width={200}
-        //   height={200}
         />
-        <Progress className={style.progress} type="circle" percent={75} width={200} />
+        <Progress
+          className={style.progress}
+          type="dashboard"
+          gapDegree={1}
+          percent={progress}
+          status='normal'
+          width={200}
+          format={() => level}
+          trailColor='rgba(0, 0, 0, 0.2)'
+        />
+      </div>
+      <div className={style.achievements}>
+        <Avatar.Group
+          maxCount={3}
+          size={45}
+          maxPopoverTrigger="click"
+          maxPopoverPlacement="bottomRight"
+          // size="large"
+          maxStyle={{ color: '#f56a00', backgroundColor: '#fde3cf', cursor: 'pointer' }}
+        >
+          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+          <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+          <Avatar style={{ backgroundColor: '#1890ff' }} icon={<AntDesignOutlined />} />
+        </Avatar.Group>
       </div>
     </div>
   );
