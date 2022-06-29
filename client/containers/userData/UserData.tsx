be6ac315a69a98aaeaf373244bad0969d8e26d87
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-06-29 16:04:28 +0100

    profile handleUrlHash
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- client/containers/userData/UserData.tsx
+++ client/containers/userData/UserData.tsx
@@ -1,5 +1,8 @@
 import style from "./userData.module.css";
+import { useEffect, useState } from "react";
 import { Tabs } from "antd";
+import { useRouter } from 'next/router'
+import Link from "next/link";
 import AccountSetting from '@/containers/accountSettingTab/AccountSettingTab'
 import FriendsTabs from '@/containers/friendsTab/FriendsTabs'
 import BlockedsList from "@/components/blockedsList/BlockedsList";
@@ -8,23 +11,49 @@ import LastMatches from "@/components/lastMatches/LastMatches";
 const { TabPane } = Tabs;
 
 const UserData: React.FC = () => {
+  const [currentTab, setCurrentTab] = useState<string>('AccountSettings')
+  const router = useRouter()
+
+  const handelUrlHash = () => {
+    const hashs = ['AccountSettings', 'Friends', 'Blockeds', 'LastMatches']
+    const hash = router.asPath.split('#')[1]
+    if (hashs.includes(hash)) setCurrentTab(hash);
+    
+  }
+
+  useEffect(() => {
+    handelUrlHash()
+    
+  }, [router.asPath])
+
+  useEffect(() => {
+    handelUrlHash()
+  }, [])
+
+
+
   return (
     <div className={style.container}>
-      <Tabs centered size="large" tabBarStyle={{
+      <Tabs centered size="large" 
+      activeKey={currentTab}
+      onChange={(key) => {
+        router.push(`#${key}`)
+      }}
+      tabBarStyle={{
         width: '100%',
         margin: 'auto',
 
       }} >
-        <TabPane tab="Account Settings" key="1">
+        <TabPane tab="Account Settings" key="AccountSettings">
           <AccountSetting />
         </TabPane>
-        <TabPane tab="Friends" key="2">
+        <TabPane tab="Friends" key="Friends">
           <FriendsTabs />
         </TabPane>
-        <TabPane tab="Blockeds" key="3">
+        <TabPane tab="Blockeds" key="Blockeds">
           <BlockedsList />
         </TabPane>
-        <TabPane tab="Last Matches" key="4">
+        <TabPane tab="Last Matches" key="LastMatches">
           <LastMatches />
         </TabPane>
       </Tabs>
