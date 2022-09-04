2a08aadc52022d1c895ec25c6491e6de31f5dd9c
alizaynoune <zaynoune.ali@gmail.com>
2022-09-04 15:54:25 +0100

    add [search and notif] in header
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- client/layouts/MasterLayout.tsx
+++ client/layouts/MasterLayout.tsx
@@ -1,8 +1,14 @@
-import layoutStyle from "./layout.module.css";
+import style from "./layout.module.css";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+  BellOutlined,
+  BellFilled,
+} from "@ant-design/icons";
 import { ReactNode, forwardRef, createRef } from "react";
 import {
   Layout,
@@ -13,35 +19,30 @@ import {
   Menu,
   MenuProps,
   Card,
+  Input,
+  Badge,
 } from "antd";
 import Link from "next/link";
 import SiderLayout from "@/components/sider/Sider";
 
 import { useAppSelector, useAppDispatch } from "@/hooks/reduxHooks";
 import { selectAuth, logout } from "@/reducers/auth";
-import logoutIcon from "@/icons/out.svg";
 import Icon from "@ant-design/icons";
-
+import logoutIcon from "@/icons/out.svg";
+import NotifIcon from "@/icons/notif.svg";
+import SearchIcon from "@/icons/search.svg";
 
 type MenuItem = Required<MenuProps>["items"][number];
-const getItem = (
-  label: React.ReactNode,
-  key: React.Key,
-  type?: "group",
-  // disabled?: boolean,
-  // children?: []
-) => {
+const getItem = (label: React.ReactNode, key: React.Key, type?: "group") => {
   return {
     label,
     key,
     type,
-    // disabled,
-    // children,
   } as MenuItem;
 };
 
 const { Header, Footer, Content, Sider } = Layout;
-const {Meta} = Card
+const { Meta } = Card;
 
 type Props = {
   children: ReactNode;
@@ -50,32 +51,38 @@ type Props = {
 
 const MasterLayout: React.FC<Props> = (props) => {
   const { children } = props;
-  const router = useRouter()
+  const router = useRouter();
   const { isAuth, avatar, email, name } = useAppSelector(selectAuth);
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
   // console.log(isAuth);
   console.log(router.asPath);
-  
 
   const items: MenuProps["items"] = [
-
     getItem(
       <Card
-      // style={{cursor: 'initial'}}
-      bordered={false}
-      actions={[
-        <Icon component={logoutIcon} style={{ fontSize: "120%" }} onClick={(e) => {dispatch(logout())}} />,
-        <Link href='/profile/me#AccountSettings'><SettingOutlined key="setting" /></Link>,
-      ]}
-    >
-      <Meta
-        avatar={<Avatar src={avatar} size='large' />}
-        title={name.first + ' ' + name.last}
-        description={email}
-      />
-    </Card>,
+        // style={{cursor: 'initial'}}
+        bordered={false}
+        actions={[
+          <Icon
+            component={logoutIcon}
+            style={{ fontSize: "120%" }}
+            onClick={(e) => {
+              dispatch(logout());
+            }}
+          />,
+          <Link href="/profile/me#AccountSettings">
+            <SettingOutlined key="setting" />
+          </Link>,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src={avatar} size="large" />}
+          title={name.first + " " + name.last}
+          description={email}
+        />
+      </Card>,
       "1",
-      "group",
+      "group"
       // true,
       // []
     ),
@@ -83,27 +90,50 @@ const MasterLayout: React.FC<Props> = (props) => {
 
   return (
     // Master layout
-    <Layout className={layoutStyle.layout}>
+    <Layout className={style.layout}>
       {/* Header */}
-      <Header className={layoutStyle.header}>
-        <Link href="/">
-          <a className={layoutStyle.logo}>
-            <Image src="/images/Logo.png" height={68} width={110} />
-          </a>
-        </Link>
+      <Header className={style.header}>
+        <div className={style.leftDiv}>
+          <Link href="/">
+            <a className={style.logo}>
+              <Image src="/images/Logo.png" height={68} width={110} />
+            </a>
+          </Link>
+          {isAuth && (
+            <Input
+              className={style.search}
+              size="large"
+              placeholder="Search"
+              suffix={
+                <Icon
+                  component={SearchIcon}
+                  style={{ fontSize: "135%", color: "var(--light-color)" }}
+                />
+              }
+            />
+          )}
+        </div>
         {!isAuth ? (
           <Button>
             <Link href="/auth/login">Login</Link>
           </Button>
         ) : (
-          // <>
-          <Dropdown overlay={<Menu items={items}  />} trigger={["click"]}
-          >
-            <a>
-              <Avatar src={avatar} size={55} />
-            </a>
-          </Dropdown>
-          // </>
+          <div className={style.rightDiv}>
+            <Badge count={3}>
+              <BellFilled
+                style={{
+                  fontSize: "180%",
+                  color: "var(--light-color)",
+                  fontWeight: "bold",
+                }}
+              />
+            </Badge>
+            <Dropdown overlay={<Menu items={items} />} trigger={["click"]}>
+              <a>
+                <Avatar src={avatar} size={55} />
+              </a>
+            </Dropdown>
+          </div>
         )}
       </Header>
       {/* Header end */}
@@ -113,40 +143,37 @@ const MasterLayout: React.FC<Props> = (props) => {
         {isAuth && <SiderLayout />}
         {/* Sider end */}
         {/* content */}
-        <Content className={layoutStyle.contentContainer}>{children}</Content>
+        <Content className={style.contentContainer}>{children}</Content>
         {/* content end */}
       </Layout>
       {/* content Layout end */}
-        { router.asPath === '/' && (
-      <div className={layoutStyle.sectionGameInfo}>
-        <div className={layoutStyle.sectionGameInfoLogo}>
-          <Link href="/">
-            <a>
-              <Image src="/images/Logo.png" height={68} width={110} />
-            </a>
-          </Link>
+      {router.asPath === "/" && (
+        <div className={style.sectionGameInfo}>
+          <div className={style.sectionGameInfoLogo}>
+            <Link href="/">
+              <a>
+                <Image src="/images/Logo.png" height={68} width={110} />
+              </a>
+            </Link>
+          </div>
+          <div className={style.sectionGameInfoText}>
+            <Typography.Title level={3} className={style.sectionGameInfoTettel}>
+              About game
+            </Typography.Title>
+            <Typography.Text className={style.sectionGameInfoText}>
+              Ping-pong is a game in which two or four players hit a light,
+              hollow ball back and forth across a net stretched across the
+              center of a table. The game is more commonly known as table
+              tennis, reflecting its origin as an indoor modification of the
+              sport of lawn tennis. The term ping-pong is a federally registered
+              trademark for the game first issued to Parker Brothers, Inc., in
+              1901, and now owned by Escalade Sports, of Evansville, Indiana.
+              Provide additional interactive capacity of editable and copyable.
+            </Typography.Text>
+          </div>
         </div>
-        <div className={layoutStyle.sectionGameInfoText}>
-          <Typography.Title
-            level={3}
-            className={layoutStyle.sectionGameInfoTettel}
-          >
-            About game
-          </Typography.Title>
-          <Typography.Text className={layoutStyle.sectionGameInfoText}>
-            Ping-pong is a game in which two or four players hit a light, hollow
-            ball back and forth across a net stretched across the center of a
-            table. The game is more commonly known as table tennis, reflecting
-            its origin as an indoor modification of the sport of lawn tennis.
-            The term ping-pong is a federally registered trademark for the game
-            first issued to Parker Brothers, Inc., in 1901, and now owned by
-            Escalade Sports, of Evansville, Indiana. Provide additional
-            interactive capacity of editable and copyable.
-          </Typography.Text>
-        </div>
-      </div>
-        )}
-      <Footer className={layoutStyle.footer}>
+      )}
+      <Footer className={style.footer}>
         <span>ft_transcendence</span>
         <span>© 2022 1337. All rights reserved.</span>
       </Footer>
