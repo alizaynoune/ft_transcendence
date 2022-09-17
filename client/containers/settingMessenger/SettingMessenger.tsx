ef63596d5d0c8e8e708afea4c3f025feee94a6f6
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-09-17 14:51:22 +0100

    add onClinkAction on card conversation settings
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- client/containers/settingMessenger/SettingMessenger.tsx
+++ client/containers/settingMessenger/SettingMessenger.tsx
@@ -1,5 +1,5 @@
 import style from "./settingMessenger.module.css";
-import { Avatar, Button, Card, Space, Typography } from "antd";
+import { Avatar, Button, Card, Space, Typography, Popover } from "antd";
 
 import { ConversationsType } from "@/types/types";
 import { ReactNode, useEffect } from "react";
@@ -16,8 +16,6 @@ import {
   MuteIcon,
   SettingIcon,
 } from "@/icons/index";
-
-import type { CustomIconComponentProps } from "@ant-design/icons/lib/components/Icon";
 import type { UserType } from "@/types/types";
 type PropsType = {
   conversation: ConversationsType;
@@ -29,12 +27,23 @@ const CardGridStyle: React.CSSProperties = {
   color: "var(--secondary-color)",
 };
 
-const CardGrid = (icon: any, label: string, key: number) => {
+const CardGrid = (
+  item: { lable: string; icon: any; hoverable: boolean },
+  key: number,
+  id: string
+) => {
   return (
-    <Card.Grid style={CardGridStyle} key={key}>
+    <Card.Grid
+      style={CardGridStyle}
+      key={key}
+      hoverable={item.hoverable}
+      onClick={(e) => {
+        console.log(item.lable, id);
+      }}
+    >
       <Space>
-        <Icon component={icon} style={{ fontSize: "130%" }} />
-        <Typography.Text>{label}</Typography.Text>
+        <Icon component={item.icon} style={{ fontSize: "130%" }} />
+        <Typography.Text>{item.lable}</Typography.Text>
       </Space>
     </Card.Grid>
   );
@@ -42,60 +51,85 @@ const CardGrid = (icon: any, label: string, key: number) => {
 
 const CardSettingText = [
   {
+    lable: "Level 42",
+    icon: LevelIcon,
+    hoverable: false,
+  },
+  {
     lable: "View profile",
     icon: UserIcon,
+    hoverable: true,
   },
   {
     lable: "Invet to play a game",
     icon: PlayGameIcon,
+    hoverable: true,
   },
-  {
-    lable: "Level 42",
-    icon: LevelIcon,
-  },
+
   {
     lable: "Delete conversation",
     icon: TrashIcon,
+    hoverable: true,
   },
   {
     lable: "Block",
     icon: BlockUserIcon,
+    hoverable: true,
   },
   {
     lable: "Mute",
     icon: SpeakerIcon,
+    hoverable: true,
   },
 ];
 
-const getCardGridItem = () => {
-  return CardSettingText.map((i, key) => CardGrid(i.icon, i.lable, key));
+const getCardGridItem = (id: string) => {
+  return CardSettingText.map((i, key) => CardGrid(i, key, id));
 };
 
+/**
+ * @param id string
+ * @description handel click member's group chat
+ */
 const handelClick = (id: string) => {
-//console.log(id);
+  console.log(id);
 };
 
 const CardMembers = (members: UserType[]) => {
   return members.map((m, key) => (
-    <Card.Grid
-      style={CardGridStyle}
+    <Popover
       key={key}
-      onClick={(e) => handelClick(m.id)}
-    >
-      <Space>
-        <Avatar src={m.avatar} size="large" />
-        <Space direction="vertical">
-          <Typography.Text>{m.name.username}</Typography.Text>
-          <Typography.Text type="secondary">{m.email}</Typography.Text>
+      content={getCardGridItem(m.id)}
+      title={
+        <Space>
+          <Avatar src={m.avatar} size="large" />
+          <Space direction="vertical">
+            <Typography.Text>{m.name.username}</Typography.Text>
+            <Typography.Text type="secondary">{m.email}</Typography.Text>
+          </Space>
         </Space>
-      </Space>
-    </Card.Grid>
+      }
+      trigger="click"
+      onVisibleChange={(e) => {
+        console.log(e);
+      }}
+    >
+      <Card.Grid style={CardGridStyle} onClick={(e) => handelClick(m.id)}>
+        <Space>
+          <Avatar src={m.avatar} size="large" />
+          <Space direction="vertical">
+            <Typography.Text>{m.name.username}</Typography.Text>
+            <Typography.Text type="secondary">{m.email}</Typography.Text>
+          </Space>
+        </Space>
+      </Card.Grid>
+    </Popover>
   ));
 };
 
 const SettingMessenger: React.FC<PropsType> = ({ conversation }) => {
   useEffect(() => {
-//console.log(conversation.id);
+    //console.log(conversation.id);
   }, [conversation]);
 
   return (
@@ -143,16 +177,13 @@ const SettingMessenger: React.FC<PropsType> = ({ conversation }) => {
         }
         extra={
           conversation.type === "group" ? (
-            <Button
-              type="link"
-              icon={<Icon component={Settings2Icon} />}
-            />
+            <Button type="link" icon={<Icon component={Settings2Icon} />} />
           ) : null
         }
       >
         {conversation.type === "group"
           ? CardMembers(conversation.members)
-          : getCardGridItem()}
+          : getCardGridItem(conversation.members[1].id)}
       </Card>
     </div>
   );
