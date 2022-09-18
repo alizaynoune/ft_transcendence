b4a55b4afddc38b514510407ef73943ec8eda000
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-09-18 18:42:35 +0100

    init threeJs-fiber
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- client/pages/game/[game].tsx
+++ client/pages/game/[game].tsx
@@ -1,10 +1,45 @@
+import style from "./game.module.css";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
-const Games : React.FC= () => {
-    return(
-        <div>
-            playGame
-        </div>
-    )
+function Box(props: any) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+    // useFrame((state, delta) => (ref.current.rotation.x  += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
 }
 
-export default Games
+const Games: React.FC = () => {
+  // useEffect(() => {
+  // }, [])
+  return (
+    <div className={style.container}>
+     <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+    </div>
+  );
+};
+
+export default Games;
