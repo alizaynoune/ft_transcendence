340fb2d24f8cb493750aabd6308f9ff18644bd0d
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-09-20 16:01:49 +0100

    update canvas & fix raquet move
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- client/pages/game/[game].tsx
+++ client/pages/game/[game].tsx
@@ -1,19 +1,25 @@
 import style from "./game.module.css";
-import { Suspense, useRef, useState, KeyboardEvent, useMemo } from "react";
+import {
+  Suspense,
+  useRef,
+  useState,
+  KeyboardEvent,
+  useMemo,
+  useEffect,
+} from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Stats, OrbitControls, Stars, Plane } from "@react-three/drei";
 import * as THREE from "three";
 import React from "react";
 
-const Racquet = (props: any) => {
+const Box = React.forwardRef((props: any, ref) => {
   return (
-    <mesh {...props}>
-      <boxGeometry attach="geometry" args={[2.4, 0.3, 0.1]} />
-      <meshStandardMaterial attach="material" color="#0391BA" />
-      {/* <meshBasicMaterial attach="material" color="#0391BA" /> */}
+    <mesh {...props.mesh} ref={ref}>
+      <boxGeometry attach="geometry" {...props.box} />
+      <meshStandardMaterial attach="material" {...props.meshMaterial} />
     </mesh>
   );
-};
+});
 
 const Ball = React.forwardRef((props: any, ref) => {
   return (
@@ -27,116 +33,115 @@ const Ball = React.forwardRef((props: any, ref) => {
 const Wall = (props: any) => {
   return (
     <Plane {...props.plane}>
-      <meshStandardMaterial
-        {...props.meshMaterial}
-        attach="material"
-        wireframe
-      />
+      <meshStandardMaterial {...props.meshMaterial} attach="material" />
     </Plane>
   );
 };
 
-const Scene = (props: any) => {
-  // const racquet = useRef<THREE.Mesh>(null!);
+const Scene = React.forwardRef((props: any, ref) => {
   const ball = useRef<THREE.Mesh>(null!);
-  // const [ballStep, setBallStep] = useState<number>(0.1)
-  const step = useRef<{x:number; y:number; z:number}>({x: 0.1, y: 0.01, z:0.05})
-  
+  const step = useRef<{ x: number; z: number }>({ x: 0.0025, z: 0.2 });
+
   useFrame(() => {
-    // ball.current!.position.x += -0.01 // Left Right
-    // ball.current!.position.y += 0.01 // Up Down
-    // if (ball.current!.position.z >= 10) setBallStep(-0.1)
-    // else if (ball.current!.position.z <= -10) setBallStep(0.1)
-    // ballStep.current += 0.1
-    // console.log(ball.current.position.z + ballStep.current);
-    
-    // step.current.y += step.current.y > 0 ? -0.01 : 0.01;
-    if (Math.abs(ball.current!.position.z) >= 10) step.current.z *= -1
-    if (Math.abs(ball.current!.position.x) >= 7) step.current.x *= -1
-    // if (Math.abs(ball.current!.position.y) >= 7) ballStep.current.y *= -1
-    // if (!(Math.round(Math.abs(step.current.y)) % 3)) step.current.y *= -1
-    
-    ball.current!.position.z += step.current.z // Front Back
-    // $x = $r * Math.cos($step*$i);
-    // step.current.y += 0.01
-    // console.log(Math.cos(step.current.y));
-    // ball.current!.position.y += Math.sin(step.current.y) * Math.cos(step.current.y) 
-    // ball.current!.position.y += step.current.y
-    // console.log(Math.abs(ball.current!.position.z));
-    // console.log(ball.current!.position.z);
-    // console.log(ballStep.current);
-    
-    
-    
-    
-    // LeftRight=|7| UpDown |10|
+    if (Math.abs(ball.current.position.z) >= 15) step.current.z *= -1;
+    if (Math.abs(ball.current.position.x) >= 7) step.current.x *= -1;
+    // console.log(ref.current.position);
+
+    // ball.current!.position.z += step.current.z; // Front Back
+    // ball.current.position.x += step.current.x; // Left Right
+    if (ball.current.position.z >= 15) {
+      // ball.current.position.z = Math.round(ball.current.position.z)
+      // console.log(`${props.racquet} ${ball.current.position.x}`);
+    }
+    // console.log(ref &&  ref.current);
+
+    // console.log(props.handleKey);
+
+    // console.log(ball.current.position.z);
   });
+
+  useEffect(() => {
+    console.log("rander");
+  }, []);
 
   return (
     <>
       <pointLight position={[10, 10, 10]} color={0xffffff} intensity={0.8} />
-      <Racquet position={[0, 0.15, -10.2]} />
-      <Ball position={[0, 0.15, 0]} ref={ball} />
-      <Racquet position={[0 + props.position, 0.15, 10.2]} />
+      {/* Raquet Player */}
+      <Box
+        mesh={{ position: [0, 0.15, -15.2] }}
+        box={{ args: [2.4, 0.3, 0.1] }}
+        meshMaterial={{ color: "#50cd89" }}
+      />
+      {/* My Raquet */}
+      <Ball position={[7, 0.2, 15]} ref={ball} />
+      <Box
+        ref={ref}
+        mesh={{ position: [0, 0.15, 15.2] }}
+        box={{ args: [2.4, 0.3, 0.1] }}
+        meshMaterial={{ color: "#3699ff" }}
+      />
       {/* Floor*/}
       <Wall
         plane={{
-          args: [14.4, 21, 40, 20],
+          args: [14.4, 31, 100, 80],
           rotation: [1.5 * Math.PI, 0, 0],
           position: [0, 0, 0],
         }}
-        meshMaterial={{ color: "#f9c74f" }}
+        meshMaterial={{ color: "#464E5F", flatShading: true }}
       />
-
       {/* Center Wall */}
-      <Wall
-        plane={{ args: [14.4, 0.5, 10, 10], position: [0, 0.25, 0] }}
-        meshMaterial={{ color: "pink" }}
+      <Box
+        mesh={{ position: [0, 0, 0] }}
+        box={{ args: [14.4, 0.1, 0.2] }}
+        meshMaterial={{ color: "#ffffff" }}
       />
       {/* Right Wall */}
       <Wall
         plane={{
-          args: [21, 1, 20, 20],
+          args: [31, 3, 80, 30],
           rotation: [0, Math.PI / 2, 0],
-          position: [7.2, 0.5, 0],
+          position: [7.2, 1.5, 0],
         }}
-        meshMaterial={{ color: "#80ffbd" }}
+        meshMaterial={{ color: "#ffffff", wireframe: true }}
       />
       {/* Left Wall */}
       <Wall
         plane={{
-          args: [21, 1, 20, 20],
+          args: [31, 3, 80, 30],
           rotation: [0, Math.PI / 2, 0],
-          position: [-7.2, 0.5, 0],
+          position: [-7.2, 1.5, 0],
         }}
-        meshMaterial={{ color: "#80ffbd" }}
+        meshMaterial={{ color: "#ffffff", wireframe: true }}
       />
     </>
   );
-};
+});
 
 const Games: React.FC = () => {
-  const [position, setPosition] = useState<number>(0);
-  const [ball, setBall] = useState<{ x: number; y: number }>({ x: 6, y: 9 });
+  // const [racquet, setRacquet] = useState<number>(0);
+  const racquet = useRef<THREE.Mesh>(null!);
   const handleKeyboardEvent = (e: KeyboardEvent<HTMLImageElement>) => {
-    console.log(position);
     const { code } = e;
-    // ArrowRight  ArrowLeft
     const step = code === "ArrowRight" ? 1 : code === "ArrowLeft" ? -1 : 0;
-    if (Math.abs(position + step) <= 6) setPosition((old) => old + step);
+    console.log(racquet.current);
+    if (Math.abs(racquet.current.position.x + step) <= 6)
+      racquet.current.position.x += step;
+    // return e
+
+    // if (Math.abs(racquet + step) <= 6) setRacquet((prev) => prev + step);
   };
-  // useEffect(() => {
-  // }, [])
   return (
     <div className={style.container}>
       <Canvas
         onKeyDown={handleKeyboardEvent}
         tabIndex={0}
-        camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 14] }}
+        camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 20] }}
         onCreated={({ gl }) => {
           gl.setClearColor("#252934");
         }}
       >
+        {/* <fog attach="fog" args={["white", 30, 30]} /> */}
         <Stats />
         <OrbitControls />
         <Suspense fallback={null}>
@@ -149,7 +154,7 @@ const Games: React.FC = () => {
             fade
             speed={1}
           />
-          <Scene position={position} ball={ball} />
+          <Scene handleKey={handleKeyboardEvent} ref={racquet} />
         </Suspense>
       </Canvas>
     </div>
