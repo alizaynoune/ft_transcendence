5cfbdff21e5f052cb6b7120f1ccc0f2cab1b496e
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-09-22 19:16:40 +0100

    working on gamePlayers info
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

---
+++ client/containers/canvas/Canvas.tsx
@@ -0,0 +1,120 @@
+import style from "./canvas.module.css";
+import { Suspense, useRef, KeyboardEvent, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stats, OrbitControls, Stars, Loader } from "@react-three/drei";
+import * as THREE from "three";
+import React from "react";
+import { NoToneMapping } from "three";
+import Scene from "@/containers/scene/Scene";
+import { racquetSize, planeSize } from "@/tools/globalVariable";
+import { useInterval } from "@/hooks/useInterval";
+import { Message } from "@/components/r3jObjects/R3jObjects";
+
+const MyCanvas: React.FC = () => {
+  const racquet = useRef<THREE.Mesh>(null!);
+  const canvasRef = useRef<HTMLCanvasElement>(null!);
+  const racquetMaxStep = planeSize[0] / 2 - racquetSize[0] / 2;
+  const [count, setCount] = useState<number>(3);
+  const [timer, setTimer] = useState(1000);
+  const [collided, setCollided] = useState<boolean>(false);
+  const [start, setStart] = useState<boolean>(false);
+
+  const gameSpeed = 0.25;
+
+  useInterval(() => setCount((count) => count - 1), timer);
+  useEffect(() => {
+    console.log(count);
+    if (count < 0) {
+      setTimer(0);
+      setStart(true);
+    }
+  }, [count]);
+
+  const handleKeyboardEvent = (e: KeyboardEvent<HTMLImageElement>) => {
+    if (count !== -1) return;
+    const { code } = e;
+    console.log(code);
+    let step = 0;
+    switch (code) {
+      case "ArrowRight":
+      case "ArrowUp":
+      case "KeyL":
+        step = 0.8;
+        break;
+      case "ArrowLeft":
+      case "ArrowDown":
+      case "KeyJ":
+        step = -0.8;
+        break;
+      default:
+        step = 0;
+    }
+    if (Math.abs(racquet.current.position.x + step) <= racquetMaxStep)
+      racquet.current.position.x += step;
+      else{
+        step = racquetMaxStep - Math.abs(racquet.current.position.x)
+        racquet.current.position.x += step;
+      }
+  };
+
+  useEffect(() => {
+    canvasRef.current.focus();
+    document.getElementById("canvas")?.focus();
+    setCount(3);
+    setTimer(1000);
+  }, []);
+
+  useEffect(() => {
+    if (!collided) return
+    setStart(false)
+    setCount(0)
+    setCollided(false)
+    setTimer(1000);
+  }, [collided])
+
+  return (
+        <Canvas className={style.container}
+          ref={canvasRef}
+          onKeyDown={handleKeyboardEvent}
+          id="canvas"
+          tabIndex={0}
+          camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 20] }}
+          gl={{ toneMapping: NoToneMapping }}
+          onCreated={({ gl }) => {
+            gl.setClearColor("#9a9a9a");
+          }}
+        >
+          <color attach="background" args={["#464E5F"]} />
+          <Stats />
+          <OrbitControls />
+          <ambientLight color={"#ffffff"} intensity={0.5} />
+          <Suspense fallback={null}>
+            <Stars
+              radius={80}
+              depth={40}
+              count={9000}
+              factor={4}
+              saturation={0}
+              fade
+              speed={1}
+            />
+            <Scene
+              ref={racquet}
+              gameSpeed={gameSpeed}
+              start={start}
+              setCollided={(value: boolean): void => {
+                setCollided(value);
+              }}
+            />
+            {count >= 0 && (
+              <Message
+                text={count ? count : "GO"}
+                mesh={{ position: [0, 3.3, 2], rotation: [-1, 0, 0] }}
+              />
+            )}
+          </Suspense>
+        </Canvas>
+  );
+};
+
+export default MyCanvas;
--- client/pages/game/[game].tsx
+++ client/pages/game/[game].tsx
@@ -1,124 +1,74 @@
 import style from "./game.module.css";
-import { Suspense, useRef, KeyboardEvent, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Stats, OrbitControls, Stars, Loader } from "@react-three/drei";
-import * as THREE from "three";
-import React from "react";
-import { NoToneMapping } from "three";
-import Scene from "@/containers/scene/Scene";
-import { racquetSize, planeSize } from "@/tools/globalVariable";
-import { useInterval } from "@/hooks/useInterval";
-import { Message } from "@/components/r3jObjects/R3jObjects";
+import React, { useEffect, useState } from "react";
+import { Avatar, Button, Space, Spin } from "antd";
+import Icon, { EyeFilled } from "@ant-design/icons";
+
+import Canvas from "@/containers/canvas/Canvas";
+
+interface GameType {
+  id: string;
+  watching: number;
+  players: { id: string; username: string; avatar: string; point: number }[];
+}
+
+async function fetchUserList() {
+  return fetch("https://randomuser.me/api/?results=2&inc=picture,login&noinfo")
+    .then((response) => response.json())
+    .then((body) =>
+      body.results.map(
+        (user: {
+          login: { username: string; uuid: string };
+          picture: { large: string };
+        }) => ({
+          id: user.login.uuid,
+          username: user.login.username,
+          avatar: user.picture.large,
+          point: Math.floor(Math.random() * 10),
+        })
+      )
+    );
+}
 
 const Games: React.FC = () => {
-  const racquet = useRef<THREE.Mesh>(null!);
-  const canvasRef = useRef<HTMLCanvasElement>(null!);
-  const racquetMaxStep = planeSize[0] / 2 - racquetSize[0] / 2;
-  const [count, setCount] = useState<number>(3);
-  const [timer, setTimer] = useState(1000);
-  const [collided, setCollided] = useState<boolean>(false);
-  const [start, setStart] = useState<boolean>(false);
-
-  const gameSpeed = 0.25;
-
-  useInterval(() => setCount((count) => count - 1), timer);
-  useEffect(() => {
-    console.log(count);
-    if (count < 0) {
-      setTimer(0);
-      setStart(true);
-    }
-  }, [count]);
-
-  const handleKeyboardEvent = (e: KeyboardEvent<HTMLImageElement>) => {
-    if (count !== -1) return;
-    const { code } = e;
-    console.log(code);
-    let step = 0;
-    switch (code) {
-      case "ArrowRight":
-      case "ArrowUp":
-      case "KeyL":
-        step = 0.8;
-        break;
-      case "ArrowLeft":
-      case "ArrowDown":
-      case "KeyJ":
-        step = -0.8;
-        break;
-      default:
-        step = 0;
-    }
-    if (Math.abs(racquet.current.position.x + step) <= racquetMaxStep)
-      racquet.current.position.x += step;
-      else{
-        step = racquetMaxStep - Math.abs(racquet.current.position.x)
-        racquet.current.position.x += step;
-      }
-  };
+  const [gameData, setGameData] = useState<GameType>();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    canvasRef.current.focus();
-    document.getElementById("canvas")?.focus();
-    setCount(3);
-    setTimer(1000);
+    setLoading(true)
+    fetchUserList()
+      .then((res) => {
+        console.log(res);
+        setGameData({
+          id: "id",
+          watching: Math.floor(Math.random() * 100),
+          players: res,
+        });
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err, "error<<<<<<");
+      });
   }, []);
 
   useEffect(() => {
-    if (!collided) return
-    setStart(false)
-    setCount(0)
-    setCollided(false)
-    setTimer(1000);
-  }, [collided])
+    console.log(gameData);
+  }, [gameData]);
 
   return (
-    <div className={style.container}>
-      <>
-        <Canvas
-          ref={canvasRef}
-          onKeyDown={handleKeyboardEvent}
-          id="canvas"
-          tabIndex={0}
-          camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 20] }}
-          gl={{ toneMapping: NoToneMapping }}
-          onCreated={({ gl }) => {
-            gl.setClearColor("#9a9a9a");
-          }}
-        >
-          <color attach="background" args={["#464E5F"]} />
-          <Stats />
-          <OrbitControls />
-          <ambientLight color={"#ffffff"} intensity={0.5} />
-          <Suspense fallback={null}>
-            <Stars
-              radius={80}
-              depth={40}
-              count={9000}
-              factor={4}
-              saturation={0}
-              fade
-              speed={1}
-            />
-            <Scene
-              ref={racquet}
-              gameSpeed={gameSpeed}
-              start={start}
-              setCollided={(value: boolean): void => {
-                setCollided(value);
-              }}
-            />
-            {count >= 0 && (
-              <Message
-                text={count ? count : "GO"}
-                mesh={{ position: [0, 3.3, 2], rotation: [-1, 0, 0] }}
-              />
-            )}
-          </Suspense>
-        </Canvas>
-        <Loader />
-      </>
-    </div>
+    <Spin spinning={loading} delay={500}>
+      <div className={style.container}>
+        {!loading && (
+          <>
+          <div>Header</div>
+            <Canvas />
+            <div style={{width: '100%', display: 'flex', justifyContent: 'space-between', padding: 10}}>
+              <Button type="primary" icon={<EyeFilled />}>{gameData?.watching}</Button>
+              <Button type="primary">Leave</Button>
+            </div>
+          </>
+        )}
+      </div>
+    </Spin>
   );
 };
 
