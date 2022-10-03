0bb59658ddc2f630f05790dfd22eef067a5b8908
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-10-03 17:15:14 +0100

    add api update user data
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- client/components/accountEdit/AccountEdit.tsx
+++ client/components/accountEdit/AccountEdit.tsx
@@ -2,7 +2,9 @@ import style from "./accountEdit.module.css";
 import { Form, Input, Button, Col, Row, DatePicker, Select } from "antd";
 import Icon from "@ant-design/icons";
 import axios from "@/config/axios";
-
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
+import { selectLoading, changeLoading } from "@/reducers/globalLoading";
+import _ from "lodash";
 // Icons
 import {
     UserIcon,
@@ -12,45 +14,104 @@ import {
     CalenderEditIcon,
 } from "@/icons/index";
 import { useEffect, useState } from "react";
-// email: "nawfal.nawfal@nawfal.nawfal2";
-// first_name: "Ali";
-// id: 1;
-// img_url: "https://cdn.intra.42.fr/users/alzaynou.jpg";
-// intra_id: 51111;
-// last_name: "Zaynoune";
-// updated_at: "2022-09-23T15:21:25.408Z";
-// username: "alzaynou";
+
 const { Option } = Select;
 const AccountSetting: React.FC = () => {
-    const [data, setData] = useState<{email: string; first_name: string; last_name: string}>();
+    const [data, setData] = useState<{
+        email: string;
+        first_name: string;
+        last_name: string;
+        username: string;
+    }>();
+    const dispatch = useAppDispatch();
+    const [form] = Form.useForm();
 
     const LoadingData = async () => {
+        dispatch(changeLoading(true));
         try {
             const res = await axios.get("profile");
-            
-            console.log(res.data);
+            setData(
+                (({ email, first_name, last_name, username }) => ({
+                    email,
+                    first_name,
+                    last_name,
+                    username
+                }))(res.data)
+            );
+            dispatch(changeLoading(false));
         } catch (error) {
             console.log(error);
+            dispatch(changeLoading(false));
         }
     };
 
     useEffect(() => {
         LoadingData();
     }, []);
+    useEffect(() => {
+      console.log(data, '>>>>>>>>>>>>>');
+      
+        form.setFieldsValue({
+            first_name: data?.first_name,
+            last_name: data?.last_name,
+            email: data?.email,
+            username: data?.username
+        });
+    }, [data]);
+
+    const onFinish = async (values: any) => {
+        // console.log(values);
+        const update = (({ email, first_name, last_name, username }) => ({
+            email,
+            first_name,
+            last_name,
+            username
+        }))(values);
+        const diff = Object.fromEntries(
+            // @ts-ignore
+            Object.entries(update).filter(([key, value]) => data[key] != value)
+        );
+        if (Object.keys(diff).length) {
+            dispatch(changeLoading(true));
+            try {
+                const res = await axios.put("users/update", {...diff});
+                console.log(res.data, '<<<<<<<<<<<<');
+                setData(
+                    (({ email, first_name, last_name, username }) => ({
+                        email,
+                        first_name,
+                        last_name,
+                        username
+                    }))(res.data)
+                );
+                dispatch(changeLoading(false));
+            } catch (error) {
+                dispatch(changeLoading(false));
+                console.log(error);
+            }
+        }
+    };
+
     return (
-        <Form className={style.form} name="accountSettings">
+        <Form
+            className={style.form}
+            name="accountSettings"
+            form={form}
+            onFinish={onFinish}
+        >
             <Row gutter={24} justify="space-around" align="middle">
                 <Col xs={{ span: 24, offset: 1 }} lg={{ span: 10, offset: 1 }}>
-                    <Form.Item name="firstname">
+                    <Form.Item name="first_name">
                         <Input
                             size="large"
                             placeholder="First Name"
+                            value={"kdfksjkj"}
                             prefix={<Icon component={UserIcon} />}
                         />
                     </Form.Item>
                 </Col>
                 <Col xs={{ span: 24, offset: 1 }} lg={{ span: 10, offset: 1 }}>
-                    <Form.Item name="laststname">
+                    <Form.Item name="last_name">
                         <Input
                             size="large"
                             placeholder="Last Name"
