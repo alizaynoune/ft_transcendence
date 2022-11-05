91c206675146938a2b20141f5453e55521f7a660
alizaynoune <zaynoune.ali@gmail.com>
2022-11-05 23:53:29 +0100

    boxMessage fix
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- client/containers/boxMessenger/BoxMessenger.tsx
+++ client/containers/boxMessenger/BoxMessenger.tsx
@@ -1,13 +1,14 @@
 import style from "./boxMessenger.module.css";
-import { Input, Button, List, message, Form } from "antd";
-import Icon, { CloseOutlined } from "@ant-design/icons";
+import { Input, Button, List, message, Form, InputRef, Divider, Avatar } from "antd";
+import Icon, { CloseOutlined, LoadingOutlined } from "@ant-design/icons";
 import MessageText from "@/components/messageText/MessageText";
 import { useState, useEffect, useRef } from "react";
 import axios from "@/config/axios";
-import { ConversationsType, MessageTextType, UserType, ConversationMemberType, ConversationsHistory } from "types/types";
+import { MessageTextType, UserType, ConversationMemberType, ConversationsHistory } from "types/types";
 import { useAppSelector } from "@/hooks/reduxHooks";
 import { selectAuth } from "@/store/reducers/auth";
 import Socket from "@/config/socket";
+import InfiniteScroll from "react-infinite-scroll-component";
 //Icons
 import { EmojiSmileIcon, SendIcon } from "@/icons/index";
 
@@ -30,21 +31,38 @@ type DataType = MessageTextType & {
 };
 const BoxMessenger: React.FC<PropsType> = ({ currentConversation }) => {
   const bottomRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<InputRef>(null);
   const [messages, setMessages] = useState<DataType[]>([]);
   const [showEmoji, setShowEmoji] = useState<boolean>(false);
   const { intra_id } = useAppSelector(selectAuth);
   const [myInfo, setMyInfo] = useState<ConversationMemberType>();
   const [form] = Form.useForm();
-  useEffect(() => {
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  // const [firstMessagId, setFirstMessageId] = useState<number>(0)
+
+  // useEffect(() => {
+  //   bottomRef.current?.scroll({
+  //     top: bottomRef.current.scrollHeight,
+  //   });
+  // }, [currentConversation]);
+
+  const loadMoreData = async () => {
+    console.log("loading.....");
     bottomRef.current?.scroll({
       top: bottomRef.current.scrollHeight,
     });
-  });
 
-  const loadMoreData = async () => {
     try {
-      const res = (await axios.get(`conversation/${currentConversation.id}/messages`)) as { data: DataType[] };
-      setMessages(res.data.reverse());
+      const pageSize = 50;
+      const url =
+        messages.length && messages[0].conversationid === currentConversation.id
+          ? `conversation/${currentConversation.id}/messages?cursor=${messages[0].id}&pageSize=${pageSize}`
+          : `conversation/${currentConversation.id}/messages?pageSize=${pageSize}`;
+      const res = (await axios.get(url)) as { data: DataType[] };
+      console.log(res.data, "loading done");
+      const reversData = res.data.reverse();
+      setMessages((prev) => [...reversData, ...prev]);
+      setHasMore(reversData.length === pageSize);
     } catch (error) {
       console.log(error);
       error instanceof Error && message.error(error.message);
@@ -52,20 +70,24 @@ const BoxMessenger: React.FC<PropsType> = ({ currentConversation }) => {
   };
 
   useEffect(() => {
+    console.log(messages, "update");
+  }, [messages]);
+
+  useEffect(() => {
     console.log(currentConversation, "current");
     setMyInfo(currentConversation.members.find((m) => m.userid === intra_id));
-
+    
     loadMoreData();
-    Socket.on("newMessage", (data) => {
-      console.log(data, "newMessage");
+    Socket.on("newMessage", (data: DataType) => {
+      setMessages((prev) => [...prev, data]);
     });
     return () => {
+      setMessages([]);
       Socket.off("newMessage");
     };
   }, [currentConversation]);
 
   const onFinish = (values: { new_message: string }) => {
-    // console.log(values);
     const { new_message } = values;
     if (new_message.length === 0) return;
     const body = {
@@ -76,9 +98,11 @@ const BoxMessenger: React.FC<PropsType> = ({ currentConversation }) => {
       const { data, error } = res;
       if (error) message.error(error.message);
       if (data) setMessages((prev) => [...prev, data]);
+      inputRef.current?.focus();
     });
     form.resetFields(["new_message"]);
     setShowEmoji(false);
+    console.log(inputRef);
   };
 
   const onEmojiClick = (event: any, emojiObject: any) => {
@@ -88,16 +112,34 @@ const BoxMessenger: React.FC<PropsType> = ({ currentConversation }) => {
 
   return (
     <div className={style.container}>
-      <div ref={bottomRef} className={style.box}>
-        {messages.map((m, key) => {
-          return <MessageText message={m} key={key} IamSender={m.members.users.intra_id === intra_id} />;
-        })}
+      <div id="scrollableBoxMessages" style={{ flexDirection: "column-reverse" }} ref={bottomRef} className={style.box}>
+        {/* {!loading && <LoadingOutlined twoToneColor="#eb2f96" style={{ fontSize: 20, color: "var(--primary-color)" }} />} */}
+        <InfiniteScroll
+          dataLength={messages.length}
+          next={loadMoreData}
+          hasMore={hasMore}
+          loader={""}
+          scrollableTarget="scrollableBoxMessages"
+          inverse={true}
+        >
+          <List
+            itemLayout="horizontal"
+            dataSource={messages}
+            renderItem={(item) => (
+              <>
+                <h3>{item.id}</h3>
+                <MessageText message={item} IamSender={item.members.users.intra_id === intra_id} />
+              </>
+            )}
+          />
+        </InfiniteScroll>
       </div>
       {showEmoji ? (
         <div className={style.Picker}>
           <CloseOutlined
             onClick={() => {
               setShowEmoji(false);
+              inputRef.current?.focus();
             }}
           />
           <Picker
@@ -124,11 +166,12 @@ const BoxMessenger: React.FC<PropsType> = ({ currentConversation }) => {
               }}
             >
               <Input
-                // disabled={!(myInfo?.active && !myInfo?.mute)}
+                ref={inputRef}
                 placeholder="Input your message"
                 className={style.Input}
                 size="large"
                 prefix={<Icon component={EmojiSmileIcon} style={{ fontSize: 20 }} onClick={() => setShowEmoji(!showEmoji)} />}
+                onFocus={() => setShowEmoji(false)}
               />
             </Form.Item>
             <Form.Item noStyle={true}>
